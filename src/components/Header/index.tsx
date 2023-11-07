16198167921f588c9dc9c02dcd3d51cbf197510c
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-11-07 00:07:54 -0300

    alterando header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { FaBars } from 'react-icons/fa'
 
 export default function Header() {
   return (
-    <header className=" bg-slate-900 flex-shrink-0 py-3 px-40 flex items-center justify-between">
+    <header className=" bg-slate-900 flex-shrink-0 py-3 px-3 lg:px-40 flex items-center justify-between">
       <h1 className="text-slate-50 font-bold text-4xl">Blog Dev</h1>
       <nav className="lg:flex items-center justify-center gap-6 hidden">
         <ul className="flex items-start justify-center gap-5">
