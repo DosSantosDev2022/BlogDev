2ffc58c40426b731b6332dbd86e315627ec32336
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-12-12 00:01:56 -0300

    fix : corrigindo bugs

--- src/app/Posts/[slug]/page.tsx
+++ src/app/Posts/[slug]/page.tsx
@@ -4,6 +4,8 @@ import { RichText } from '@/app/Posts/components/rich-text'
 import Image from 'next/image'
 import { format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import SmallCard from '@/components/Cards/SmallCard'
+import { ToShare } from '@/components/Posts/toShare'
 
 const GetPageData = async (): Promise<GetAllPostsTypes> => {
   const query = `
@@ -47,13 +49,13 @@ export default async function PagePost({
 
   return (
     <main className="grid  lg:grid-cols-12 gap-1 items-start justify-center mt-12 mb-12 ">
-      <section className="flex flex-col items-center justify-start lg:col-span-8 ">
+      <section className="flex flex-col items-center justify-start lg:col-span-8 px-2 ">
         <div className="w-full h-12 rounded-[10px] py-3 px-4 bg-slate-900 text-start">
           <p className="text-slate-50">{/* Renderiza o caminho do post */}</p>
         </div>
         <article className="mt-12 flex flex-col items-center justify-start w-full gap-10 p-2">
           <div className="flex flex-col items-start justify-center w-full gap-5">
-            <h1 className="text-slate-900 text-5xl font-bold mb-3">
+            <h1 className="text-slate-900 md:text-5xl text-3xl font-bold mb-3">
               {post?.title}
             </h1>
             <div className="flex items-center justify-center gap-2">
@@ -61,8 +63,8 @@ export default async function PagePost({
                 width={100}
                 height={100}
                 quality={100}
-                src={post?.author.photo.url}
-                alt={post?.author.name}
+                src={post?.author.photo.url || ''}
+                alt={post?.author.name || ''}
                 className="rounded-full w-12 h-12"
               />
               <div className="flex flex-col gap-1">
@@ -84,12 +86,12 @@ export default async function PagePost({
           <div className="w-full border">
             {post?.coverImage.url ? (
               <Image
-                width={1500}
-                height={1200}
-                alt={''}
+                width={2000}
+                height={2000}
+                alt={post.title}
                 src={post?.coverImage.url}
                 quality={100}
-                className="bg-contain w-full"
+                className="bg-contain w-full rounded-md"
               />
             ) : (
               <p>Imagem não disponível</p>
@@ -98,13 +100,25 @@ export default async function PagePost({
           <div className="w-full p-2 text-slate-600 space-y-5">
             <RichText content={post?.content.raw} />
           </div>
+          <ToShare url="" />
         </article>
       </section>
-      <section className="lg:col-span-4 flex flex-col items-start justify-center gap-5 mt-5 lg:mt-0 ">
+      <section className="lg:col-span-4 flex flex-col items-center justify-center px-2 gap-5 mt-5 lg:mt-0 ">
         <div className="w-[22.5rem] h-12 rounded-[10px] py-3   bg-slate-900 flex items-center justify-center">
           <h3 className="text-slate-50 font">Posts Relacionados</h3>
         </div>
-        <div className="flex flex-col items-center justify-center space-y-5"></div>
+        <div className="flex flex-col items-center justify-center space-y-5">
+          {posts.map((post) => (
+            <SmallCard
+              slug={post.slug}
+              key={post.id}
+              author={post.author}
+              coverImage={post.coverImage}
+              title={post.title}
+              createdAd={post.createdAt}
+            />
+          ))}
+        </div>
       </section>
     </main>
   )
--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,7 +1,6 @@
-import { format } from 'date-fns'
-import { ptBR } from 'date-fns/locale'
-import Image from 'next/image'
 import Link from 'next/link'
+import { Author } from '../Posts/author'
+import { TagsPost } from './tags'
 
 interface CardPostsProps {
   title: string
@@ -33,36 +32,22 @@ export function CardPosts({
         pathname: `/Posts/${slug}`,
       }}
     >
-      <div className=" w-[21rem] h-[24.75rem]  rounded-md  shadow-md">
+      <div className=" w-[21rem] h-[24.75rem]  rounded-md  shadow-md ">
         <div
-          className="flex flex-col justify-end h-full p-4 gap-3 rounded-md bg-center bg-auto  bg-no-repeat"
+          className="flex flex-col justify-end h-full p-4 gap-3 rounded-md bg-center  bg-cover  bg-no-repeat "
           style={{ backgroundImage: `url(${coverImage.url})` }}
         >
           <div className="flex flex-col items-start gap-1">
-            <span className="bg-slate-900 text-slate-50 text-[0.625rem] p-[0.375rem] rounded-lg">
-              {tag}
-            </span>
+            <TagsPost tagName={tag} />
             <h2 className="text-base font-bold text-slate-50">{title}</h2>
-            <p className="text-sm text-slate-100 font-normal">{''}</p>
           </div>
-          <div className="flex items-start justify-center gap-2">
-            <Image
-              width={100}
-              height={100}
-              quality={100}
-              src={author.photo.url}
-              alt={author.name}
-              className="rounded-full w-12 h-12"
-            />
-            <div className="flex flex-col gap-1">
-              <p className="text-slate-50">{author.name}</p>
-              <p className="text-slate-50">
-                {format(new Date(createdAd), "dd 'de' MMM 'de' yyyy", {
-                  locale: ptBR,
-                })}
-              </p>
-            </div>
-          </div>
+          <Author
+            CreateAd={createdAd}
+            ImageProfile={author.photo.url}
+            Name={author.name}
+            nameStyles="text-slate-50 text-sm"
+            dataStyles="text-slate-50 text-sm font-"
+          />
         </div>
       </div>
     </Link>
