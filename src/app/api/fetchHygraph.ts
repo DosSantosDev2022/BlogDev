c4ce6647a5957a07a561570d8b0762237c2d793a
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-01-09 19:51:15 -0300

    feat : adding environment variable

--- src/app/api/fetchHygraph.ts
+++ src/app/api/fetchHygraph.ts
@@ -1,16 +1,13 @@
 export const fetchHygraphQuery = async (query: string) => {
-  const response = await fetch(
-    'https://api-sa-east-1.hygraph.com/v2/clpsquqiv0hjn01t93xcg9vtl/master',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({ query }),
-      cache: 'no-store',
+  const response = await fetch(process.env.HYGRAPH_API_KEY || '', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
     },
-  )
+    body: JSON.stringify({ query }),
+    cache: 'no-store',
+  })
 
   const { data } = await response.json()
   return data
