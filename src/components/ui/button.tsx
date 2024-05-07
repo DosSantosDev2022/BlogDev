fb3ed0daf5a6884c9e80984006b00d7cf115971b
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-05-07 01:14:19 -0300

    styles : adicona variante disable

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        disabled: 'bg-transparent border border-zinc-700 text-zinc-50 ',
       },
       size: {
         default: 'h-10 px-4 py-2',
