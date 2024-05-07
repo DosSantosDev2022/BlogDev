25126452e5e38c4a201de6b784d4badeb666fa78
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-05-06 22:58:48 -0300

    config: altera plugins do tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,7 +74,11 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar')],
+  plugins: [
+    require('tailwindcss-animate'),
+    require('tailwind-scrollbar'),
+    { nocompatible: true },
+  ],
 } satisfies Config
 
 export default config
