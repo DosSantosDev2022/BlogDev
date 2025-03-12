467c8b8c22eebc365b1f9fac90e170588dfea550
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2025-03-11 21:03:26 -0300

    fix: corrigindo anuncios do google

--- src/components/globals/google/AdBanner.tsx
+++ src/components/globals/google/AdBanner.tsx
@@ -9,31 +9,27 @@ interface AdBannerTypes {
 
 export function AdBanner({ dataAdFormat, dataAdSlot }: AdBannerTypes) {
 	useEffect(() => {
-		const timeoutId = setTimeout(() => {
-			try {
-				const ads =
-					(window as unknown as { adsbygoogle: unknown[] }).adsbygoogle ||
-					[]
-				ads.push({})
-			} catch (error) {
-				console.error(error)
+		if (typeof window !== 'undefined') {
+			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+			const adsbygoogle = (window as any).adsbygoogle || []
+			if (adsbygoogle.push) {
+				try {
+					adsbygoogle.push({})
+				} catch (error) {
+					console.error('Erro ao carregar o AdSense:', error)
+				}
 			}
-		}, 500)
-
-		return () => clearTimeout(timeoutId)
+		}
 	}, [])
 
 	return (
-		<div className='border p-2'>
-			<ins
-				key={dataAdSlot}
-				className='adsbygoogle bg-transparent w-full  flex justify-center items-center'
-				style={{ display: 'block' }}
-				data-ad-client='ca-pub-2827166560948178'
-				data-ad-slot={dataAdSlot}
-				data-ad-format={dataAdFormat}
-				data-full-width-responsive='true'
-			/>
-		</div>
+		<ins
+			className='adsbygoogle bg-transparent w-full  flex justify-center items-center'
+			style={{ display: 'block' }}
+			data-ad-client='ca-pub-2827166560948178'
+			data-ad-slot={dataAdSlot}
+			data-ad-format={dataAdFormat}
+			data-full-width-responsive='true'
+		/>
 	)
 }
